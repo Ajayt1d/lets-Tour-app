7ccb2aaf45d93a897d58510466b5f7a002d1fea8
AJ <aidenjgrant@gmail.com>
2024-11-16 20:43:01 +0000

    Initial Commit

---
+++ lets Tour/lets_TourApp.swift
@@ -0,0 +1,17 @@
+//
+//  lets_TourApp.swift
+//  lets Tour
+//
+//  Created by AJAY Grant on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct lets_TourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lets TourTests/lets_TourTests.swift
@@ -0,0 +1,17 @@
+//
+//  lets_TourTests.swift
+//  lets TourTests
+//
+//  Created by AJAY Grant on 16/11/2024.
+//
+
+import Testing
+@testable import lets_Tour
+
+struct lets_TourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
